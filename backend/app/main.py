5453664d580e62325b1dd740bc291c768c5e6ee5
meghanimariam-KIZ <meghanimariam@gmail.com>
2026-04-30 23:44:28 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://dna-sequence-analyzer-five.vercel.app",
+        "https://dna-sequence-analyzer-five.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
