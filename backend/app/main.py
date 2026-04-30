8bf2841720f66667a23ecaac6b2642f7a092784f
meghanimariam-KIZ <meghanimariam@gmail.com>
2026-04-30 23:57:34 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,11 +26,11 @@ def home():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://dna-sequence-analyzer-five.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://dna-sequence-analyzer-five.vercel.app/",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
